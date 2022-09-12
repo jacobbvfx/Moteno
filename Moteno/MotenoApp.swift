9caa3bb089f70f028b0e7e26e416662bd48628f9
Jacob <38792138+shadoweG@users.noreply.github.com>
2022-09-12 08:14:24 +0200

    Initial Commit

---
+++ Moteno/MotenoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotenoApp.swift
+//  Moteno
+//
+//  Created by jacob on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MotenoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
